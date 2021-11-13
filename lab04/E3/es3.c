e91a4e881ec40f4f55ad805639a487884ec9554e
Andrea Grillo <grilloandrea6@gmail.com>
2021-11-13 08:08:17 +0100

    lab4 e3 working

--- lab04/E3/es3.c
+++ lab04/E3/es3.c
@@ -19,8 +19,9 @@ void main() {
 	int n,i,sol_dim,*sol,dati[4];
 
 	printf("Inserisci il nome del file da leggere: ");
-	scanf("%s",filename);
-	// just for testing purposes - strcpy(filename,"hard_test_set.txt");
+	//scanf("%s",filename);
+	// just for testing purposes - 
+	strcpy(filename,"easy_test_set.txt");
 
 	fp = fopen(filename,"r");
 	if(fp == NULL) {
@@ -64,7 +65,19 @@ void main() {
 
 
 int checkSol(int *sol, int to_check, int sol_dim) {
+		     if((sol[to_check] == zaffiro && !(sol[0] == zaffiro || sol[0] == rubino)) ||
+			(sol[to_check] == smeraldo && !(sol[0] == smeraldo || sol[0] == topazio)) ||
+			(sol[to_check] == rubino && !(sol[0] == smeraldo || sol[0] == topazio)) ||
+			(sol[to_check] == topazio && !(sol[0] == zaffiro || sol[0] == rubino)) ) {
+				return 0;
+			}
+return 1;
+// STOP
+
+
 	for(int i = 0; i < to_check; i++) {
+		//printf("to_check=%d,comparing %d %d indexes\n",to_check,i,(i+1)%sol_dim);
+
 		if((sol[i] == zaffiro && !(sol[(i+1)%sol_dim] == zaffiro || sol[(i+1)%sol_dim] == rubino)) ||
 			(sol[i] == smeraldo && !(sol[(i+1)%sol_dim] == smeraldo || sol[(i+1)%sol_dim] == topazio)) ||
 			(sol[i] == rubino && !(sol[(i+1)%sol_dim] == smeraldo || sol[(i+1)%sol_dim] == topazio)) ||
@@ -83,14 +96,106 @@ int checkSol(int *sol, int to_check, int sol_dim) {
 // così si generano prima quelle più grandi
 
 
-int recursion(int *sol, int pos, int k, int *max_found) {
+/*int recursion(int *sol, int pos, int k, int *max_found) {
 	int found;
 	if(pos >= k && checkSol(sol,pos,pos)) {
 		if (found > max_found) max_found = found;
 	}
 
+}*/
+
+
+
+
+int recursion(int *sol, int pos, int max_dim, int max_found, int dati[], int prova	) {
+//	pietre p;
+
+//	if(pos > 0)
+//	{
+		//printf("SOLUTION: ");
+		//for(int i = 0; i < pos; i++) printf("%d ", sol[i]);
+		//printf("\n"); 
+
+//		if (checkSol(sol,pos-1,pos-1)) {
+		//	printf("VALID SOLUTION\n");
+//			if(pos + 1 > max_found) max_found = pos + 1;
+////		}
+		//return max_found;
+//	}
+//printf("depth = %d\n",prova);
+			if(pos + 1 > max_found && checkSol(sol,pos-1,pos-1	)) max_found = pos + 1;
+
+	if(pos < max_dim) {// && checkSol(sol,pos-1,pos)) {
+		if(pos > 0) {
+			int chosenOne = 0;
+		switch (sol[pos-1]) {
+			case rubino:
+			case smeraldo:
+				if(dati[smeraldo] > 0) {
+					chosenOne = 1;
+					sol[pos] = smeraldo;
+					dati[smeraldo]--;
+					max_found = recursion(sol,pos + 1, max_dim, max_found, dati, prova + 1);
+					dati[smeraldo]++;
+				}
+
+				if(dati[topazio] > 0) {
+					chosenOne = 1;
+					sol[pos] = topazio;
+					dati[topazio]--;
+					max_found = recursion(sol,pos + 1, max_dim, max_found, dati, prova + 1);
+					dati[topazio]++;
+				}
+				break;
+
+			case zaffiro:
+			case topazio:
+				if(dati[zaffiro] > 0) {
+					chosenOne = 1;
+					sol[pos] = zaffiro;
+					dati[zaffiro]--;
+					max_found = recursion(sol,pos + 1, max_dim, max_found, dati, prova + 1);
+					dati[zaffiro]++;
+				}
+
+				if(dati[rubino] > 0) {
+					chosenOne = 1;
+					sol[pos] = rubino;
+					dati[rubino]--;
+					max_found = recursion(sol,pos + 1, max_dim, max_found, dati, prova + 1);
+					dati[rubino]++;
+				}
+				break;
+		}if(!chosenOne) {return max_found;} }else if(pos == 0) for(pietre p = zaffiro; p <= smeraldo; p++) {
+			if(dati[p] > 0) {
+				sol[pos] = p;
+				dati[p]--;
+				max_found = recursion(sol,pos + 1, max_dim, max_found, dati, prova + 1);
+				dati[p]++;
+			}
+		}
+
+	}
+
+	return max_found;
 }
 
+
+		/*for(p = zaffiro; p <= smeraldo; p++) {
+			if(dati[p] > 0) {
+
+				// pruning dinamico
+				if( ((p == rubino || p == zaffiro)		&& !(sol[pos-1] == zaffiro	|| sol[pos-1] == topazio)) ||
+					((p == smeraldo || p == topazio)	&& !(sol[pos-1] == smeraldo	|| sol[pos-1] == rubino)))
+{						//printf("\n\ncontinuing");
+continue;}
+
+				sol[pos] = p;
+				dati[p]--;
+				max_found = recursion(sol,pos + 1, max_dim, max_found, dati, prova + 1);
+				dati[p]++;
+			}
+		}*/
 
 /*
 int recursion(int *sol, int pos, int max_dim, int max_found, int dati[], int prova) {
