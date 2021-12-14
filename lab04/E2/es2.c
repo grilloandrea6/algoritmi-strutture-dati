29ec99f7f2457b33417a7c9b158b1f59a53bfa38
Andrea Grillo <grilloandrea6@gmail.com>
2021-12-14 18:15:29 +0100

    minor modifications es2 lab4

--- lab04/E2/es2.c
+++ lab04/E2/es2.c
@@ -1,8 +1,3 @@
-/*
- *
- *
- */
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -66,7 +61,6 @@ void main() {
 
 		switch (cmd) {
 			case c_nuovo:
-			printf("nuovo\n");
 				head = f_nuovo(head);
 				break;
 			case c_leggi:
